72cd7e52496f35352a74c5a16bb485207383fcac
Inan Evin <inanevin@gmail.com>
2025-01-25 01:23:33 +0200

    update sdk

--- GameProject/LinaCore/include/Core/Physics/PhysicsWorld.hpp
+++ GameProject/LinaCore/include/Core/Physics/PhysicsWorld.hpp
@@ -28,84 +28,98 @@ SOFTWARE.
 
 #pragma once
 
-#include "Core/Physics/PhysicsLayerFilter.hpp"
 #include "Core/Physics/PhysicsBPLayerInterface.hpp"
+#include "Core/Physics/PhysicsLayerFilter.hpp"
 #include "Core/Physics/PhysicsObjectBPLayerFilter.hpp"
 #include "Core/Physics/PhysicsRayCollector.hpp"
 
-#include <Jolt/Jolt.h>
-#include <Jolt/Physics/PhysicsSystem.h>
-#include <Jolt/Physics/Collision/ContactListener.h>
-#include <Jolt/Physics/Body/BodyActivationListener.h>
-#include <Jolt/Core/TempAllocator.h>
 #include <Jolt/Core/JobSystemThreadPool.h>
+#include <Jolt/Core/TempAllocator.h>
+#include <Jolt/Jolt.h>
+#include <Jolt/Physics/Body/BodyActivationListener.h>
+#include <Jolt/Physics/Collision/ContactListener.h>
+#include <Jolt/Physics/PhysicsSystem.h>
 
-namespace Lina
-{
-	class EntityWorld;
-	class Entity;
-	class PhysicsContactListener;
+namespace Lina {
+class EntityWorld;
+class Entity;
+class PhysicsContactListener;
 
-	class PhysicsWorld : public JPH::ContactListener, public JPH::BodyActivationListener
-	{
-	public:
-		PhysicsWorld(EntityWorld* world);
-		virtual ~PhysicsWorld();
+class PhysicsWorld : public JPH::ContactListener,
+                     public JPH::BodyActivationListener {
+public:
+  PhysicsWorld(EntityWorld *world);
+  virtual ~PhysicsWorld();
 
-		void SetPhysicsSettings(const WorldPhysicsSettings& opts);
-		void Begin();
-		void End();
-		void EnsurePhysicsBodies();
+  void SetPhysicsSettings(const WorldPhysicsSettings &opts);
+  void Begin();
+  void End();
+  void AddEntities(const Vector<Entity *> &e);
+  void EnsurePhysicsBodies();
 
-		void AddAllBodies();
-		void RemoveAllBodies();
-		void Simulate(float dt);
+  void AddBodies(const Vector<Entity *> &e);
+  void RemoveAllBodies();
+  void Simulate(float dt);
 
-		Entity* GetEntityFromBodyID(const JPH::BodyID& id);
+  Entity *GetEntityFromBodyID(const JPH::BodyID &id);
 
-		bool CastRayFast(const Vector3& position, const Vector3& normDirection, float maxDistance, RayResult& outRayResult);
-		bool CastRay(const Vector3& position, const Vector3& normDirection, float maxDistance, RayResult& outRayResult);
-		bool CastRayAll(const Vector3& position, const Vector3& normDirection, float maxDistance, RayResult& outRayResult);
+  bool CastRayFast(const Vector3 &position, const Vector3 &normDirection,
+                   float maxDistance, RayResult &outRayResult);
+  bool CastRay(const Vector3 &position, const Vector3 &normDirection,
+               float maxDistance, RayResult &outRayResult);
+  bool CastRayAll(const Vector3 &position, const Vector3 &normDirection,
+                  float maxDistance, RayResult &outRayResult);
 
-		// Contact
-		void AddContactListener(PhysicsContactListener* list);
-		void RemoveContactListener(PhysicsContactListener* list);
+  // Contact
+  void AddContactListener(PhysicsContactListener *list);
+  void RemoveContactListener(PhysicsContactListener *list);
 
-		JPH::Body* CreateBodyForEntity(Entity* e);
-		void	   DestroyBodyForEntity(Entity* e);
+  JPH::Body *CreateBodyForEntity(Entity *e);
+  void DestroyBodyForEntity(Entity *e);
 
-		// Contacts
-		virtual JPH::ValidateResult OnContactValidate(const JPH::Body& inBody1, const JPH::Body& inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult& inCollisionResult) override;
-		virtual void				OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override;
-		virtual void				OnContactPersisted(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override;
-		virtual void				OnContactRemoved(const JPH::SubShapeIDPair& inSubShapePair) override;
+  // Contacts
+  virtual JPH::ValidateResult
+  OnContactValidate(const JPH::Body &inBody1, const JPH::Body &inBody2,
+                    JPH::RVec3Arg inBaseOffset,
+                    const JPH::CollideShapeResult &inCollisionResult) override;
+  virtual void OnContactAdded(const JPH::Body &inBody1,
+                              const JPH::Body &inBody2,
+                              const JPH::ContactManifold &inManifold,
+                              JPH::ContactSettings &ioSettings) override;
+  virtual void OnContactPersisted(const JPH::Body &inBody1,
+                                  const JPH::Body &inBody2,
+                                  const JPH::ContactManifold &inManifold,
+                                  JPH::ContactSettings &ioSettings) override;
+  virtual void
+  OnContactRemoved(const JPH::SubShapeIDPair &inSubShapePair) override;
 
-		// Body
-		virtual void OnBodyActivated(const JPH::BodyID& inBodyID, uint64 inBodyUserData) override;
-		virtual void OnBodyDeactivated(const JPH::BodyID& inBodyID, uint64 inBodyUserData) override;
+  // Body
+  virtual void OnBodyActivated(const JPH::BodyID &inBodyID,
+                               uint64 inBodyUserData) override;
+  virtual void OnBodyDeactivated(const JPH::BodyID &inBodyID,
+                                 uint64 inBodyUserData) override;
 
-		inline JPH::PhysicsSystem& GetPhysicsSystem()
-		{
-			return m_physicsSystem;
-		}
+  inline JPH::PhysicsSystem &GetPhysicsSystem() { return m_physicsSystem; }
 
-		inline const Vector<Entity*>& GetAddedBodies() const
-		{
-			return m_addedBodies;
-		}
+  inline const Vector<Entity *> &GetAddedBodies() const {
+    return m_addedBodies;
+  }
 
-	private:
-		JPH::PhysicsSystem		 m_physicsSystem;
-		JPH::TempAllocatorImpl	 m_tempAllocator = JPH::TempAllocatorImpl(10 * 1024 * 1024);
-		JPH::JobSystemThreadPool m_jobSystem	 = JPH::JobSystemThreadPool(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers, JPH::thread::hardware_concurrency() - 1);
+private:
+  JPH::PhysicsSystem m_physicsSystem;
+  JPH::TempAllocatorImpl m_tempAllocator =
+      JPH::TempAllocatorImpl(10 * 1024 * 1024);
+  JPH::JobSystemThreadPool m_jobSystem =
+      JPH::JobSystemThreadPool(JPH::cMaxPhysicsJobs, JPH::cMaxPhysicsBarriers,
+                               JPH::thread::hardware_concurrency() - 1);
 
-		PhysicsLayerFilter				m_layerFilter;
-		PhysicsObjectBPLayerFilter		m_objectBPLayerFilter;
-		PhysicsBPLayerInterface			m_bpLayerInterface;
-		EntityWorld*					m_world = nullptr;
-		Vector<Entity*>					m_addedBodies;
-		PhysicsRayCollector				m_rayCollector;
-		PhysicsBroadphaseCollector		m_broadphaseCollector;
-		Vector<PhysicsContactListener*> m_contactListeners;
-	};
+  PhysicsLayerFilter m_layerFilter;
+  PhysicsObjectBPLayerFilter m_objectBPLayerFilter;
+  PhysicsBPLayerInterface m_bpLayerInterface;
+  EntityWorld *m_world = nullptr;
+  Vector<Entity *> m_addedBodies;
+  PhysicsRayCollector m_rayCollector;
+  PhysicsBroadphaseCollector m_broadphaseCollector;
+  Vector<PhysicsContactListener *> m_contactListeners;
+};
 } // namespace Lina
