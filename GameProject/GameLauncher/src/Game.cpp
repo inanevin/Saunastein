31638005ac1d0f9087196306c79704f7bb8442cc
Toni Pesola <toni@tonipesola.com>
2025-01-26 01:17:35 +0200

    damage stuff

--- GameProject/GameLauncher/src/Game.cpp
+++ GameProject/GameLauncher/src/Game.cpp
@@ -302,7 +302,7 @@ namespace Lina
 
 	void Game::OnContactBegin(Entity* e1, Entity* e2, const Vector3& p1, const Vector3& p2)
 	{
-    m_waveManager->HandleContact(e1, e1);
+    m_waveManager->HandleContact(e1, e2);
 	}
 
 	void Game::OnContact(Entity* e1, Entity* e2, const Vector3& p1, const Vector3& p2)
